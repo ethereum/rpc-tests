4928ef0112db48a8f2722d5cb405bf45b06f0c3f
Fabian Vogelsteller <fabian@frozeman.de>
2015-03-09 10:36:06 +0100

    add new example test

--- test/net_listening.js
+++ test/net_listening.js
@@ -7,14 +7,14 @@ var asyncTest = function(host, done){
     Helpers.send(host, {
         id: config.rpcMessageId++,
         jsonrpc: "2.0",
-        method: "eth_coinbase",
+        method: "net_listening",
         params: []
 
     }, function(result, status) {
         
         assert.equal(status, 200);
         assert.property(result, 'result', (result.error) ? result.error.message : 'error');
-        assert.deepEqual(result.result, '0x165ccc179226690230d2952beaf9849e67c231a2');
+        assert.deepEqual(result.result, true);
 
         done();
 
@@ -22,10 +22,10 @@ var asyncTest = function(host, done){
 };
 
 
-describe('eth_coinbase', function(){
+describe('net_listening', function(){
     for (var key in config.hosts) {
         describe(key.toUpperCase(), function(){
-            it('should return a coinbase address', function(done){
+            it('should return a boolean', function(done){
                 asyncTest(config.hosts[key], done);
             });
         });
