35b5a0d8c6cce5b62566b9b933cf94af52b7fe3d
Fabian Vogelsteller <fabian@frozeman.de>
2015-03-19 14:03:21 +0100

    fixed parent hash?

--- test/eth_getBlockBy.js
+++ test/eth_getBlockBy.js
@@ -11,11 +11,11 @@ var block1 = _.find(config.testBlocks.blocks, function(bl){
         return (bl.blockHeader.number == 6) ? bl : false;
     });
 block1.blockHeader.parentblockHeader = _.find(config.testBlocks.blocks, function(bl){
-    return (bl.blockHeader.number == 5) ? bl : false;
+    return (bl.blockHeader.number == 5) ? bl.blockHeader : false;
 });
 
 var block2 = _.find(config.testBlocks.blocks, function(bl){
-        return (bl.blockHeader.number == 3) ? bl.blockHeader : false;
+        return (bl.blockHeader.number == 3) ? bl : false;
     });
 block2.blockHeader.parentblockHeader = _.find(config.testBlocks.blocks, function(bl){
     return (bl.blockHeader.number == 2) ? bl.blockHeader : false;
