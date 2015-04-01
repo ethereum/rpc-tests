7ac3cdf86a6e1ff58fe6243f63807062832cdae7
Fabian Vogelsteller <fabian@frozeman.de>
2015-04-01 16:11:24 +0200

    add output to connection error

---
+++ test/eth_protocolVersion.js
@@ -0,0 +1,35 @@
+var config = require('../lib/config'),
+    Helpers = require('../lib/helpers'),
+    assert = require('chai').assert;
+
+// METHOD
+var method = 'eth_protocolVersion';
+
+// TEST
+var asyncTest = function(host, done){
+    Helpers.send(host, {
+        id: config.rpcMessageId++, jsonrpc: "2.0", method: method,
+        
+        // PARAMETERS
+        params: []
+
+    }, function(result, status) {
+        assert.equal(status, 200, 'has status code');
+        assert.property(result, 'result', (result.error) ? result.error.message : 'error');
+        assert.isString(result.result, 'is string');
+
+        done();
+    });
+};
+
+
+describe(method, function(){
+    Helpers.eachHost(function(key, host){
+        describe(key, function(){
+            it('should return a version number as HEX string', function(done){
+                asyncTest(host, done);
+            });
+        });
+    });
+});
+
