35434b37f3dbbb47976a4578c7f3c6072d5c9b25
Fabian Vogelsteller <fabian@frozeman.de>
2015-04-02 16:43:30 +0200

    fixed uncle test

--- test/eth_getUncleBy.js
+++ test/eth_getUncleBy.js
@@ -61,7 +61,7 @@ describe(method1, function(){
                 syncTest(host, method1, ['0xd2f1575105fd2272914d77355b8dab5afbdde4b012abd849e8b32111be498b0d', '0xb', false], null);
             });
             it('should return an error when the wrong parameters is passed', function(done){
-                asyncErrorTest(host, done, method1, ['0xd2f1575105fd2272914d77355b8dab5afbdde4b012abd849e8b32111be498b0d', '0xb']);
+                asyncErrorTest(host, done, method1, ['0xd2f1575105fd2272914d77355b8dab5afbdde4b012abd849e8b32111be498b0d']);
             });
             it('should return an error when no parameter is passed', function(done){
                 asyncErrorTest(host, done, method1, []);
@@ -89,7 +89,7 @@ describe(method2, function(){
                 syncTest(host, method2, ['0x2', '0xbbb', false], null);
             });
             it('should return an error when the wrong parameters is passed', function(done){
-                asyncErrorTest(host, done, method2, ['0xd2f1575105fd2272914d77355b8dab5afbdde4b012abd849e8b32111be498b0d', '0xb']);
+                asyncErrorTest(host, done, method2, ['0xd2f1575105fd2272914d77355b8dab5afbdde4b012abd849e8b32111be498b0d']);
             });
             it('should return an error when no parameter is passed', function(done){
                 asyncErrorTest(host, done, method2, []);
